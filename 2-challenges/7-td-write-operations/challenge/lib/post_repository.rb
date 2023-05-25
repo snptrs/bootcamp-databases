5ce1035a233432df2cb104de5321172b1a9b9583
Sean Peters <sean@snptrs.dev>
2023-05-25 13:52:22 +0100

    Passing test for Post#find

--- 2-challenges/7-td-write-operations/challenge/lib/post_repository.rb
+++ 2-challenges/7-td-write-operations/challenge/lib/post_repository.rb
@@ -32,8 +32,22 @@ class PostRepository
   def find(id)
     # Executes the SQL query:
     # SELECT id, title, content, view_count_ user_id FROM posts WHERE id = $1;
+    sql = "SELECT id, title, content, view_count, user_id FROM posts WHERE id = $1;"
+    result_set = DatabaseConnection.exec_params(sql, [id])
 
-    # Returns a single Post object.
+    posts = []
+
+    result_set.each do |record|
+      post = Post.new
+      post.id = record['id']
+      post.title = record['title']
+      post.content = record['content']
+      post.view_count = record['view_count']
+      post.user_id = record['user_id']
+
+      posts << post
+    end
+    return posts
   end
 
   def create(post)
--- 2-challenges/7-td-write-operations/challenge/spec/post_repository_spec.rb
+++ 2-challenges/7-td-write-operations/challenge/spec/post_repository_spec.rb
@@ -24,5 +24,16 @@ RSpec.describe PostRepository do
       expect(post[0].user_id).to eq '1'
     end
 
+    it "returns a post by id" do
+      post_repo = PostRepository.new
+      post = post_repo.find(2)
+
+      expect(post[0].id).to eq '2'
+      expect(post[0].title).to eq 'Post 2'
+      expect(post[0].content).to eq 'Lorem ipsum dolor sit amet.'
+      expect(post[0].view_count).to eq '5'
+      expect(post[0].user_id).to eq '2'
+    end
+
   end
 end
